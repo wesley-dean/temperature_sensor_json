5df85a2a9bc20f6aadfb212b555fe87407247a19
Wes Dean <>
2021-01-27 18:31:39 -0500

    Import Flask, pre-commit checks

--- dht11.py
+++ dht11.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
+
 """ read a DHT11 temperature / humidity sensor and spit it out as JSON """
 
 import json
 import time
+from flask import Flask
 import Adafruit_DHT
 
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
 
 
-def main():
-    """ the main function """
+app = Flask(__name__)
+
+
+@app.route("/")
+def read_sensor(pin=4, sensor=None):
+
+    if sensor is None:
+        sensor = Adafruit_DHT.DHT11
 
     result = {}
 
-    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    humidity, temperature = Adafruit_DHT.read(sensor, pin)
 
     if humidity is not None and temperature is not None:
 
@@ -24,7 +32,13 @@ def main():
         result["localtime"] = time.asctime(time.localtime())
         result["gm"] = time.asctime(time.gmtime())
 
-        print(json.dumps(result))
+    return json.dumps(result)
+
+
+def main():
+    """ the main function """
+
+    print(read_sensor(DHT_PIN, DHT_SENSOR))
 
 
 if __name__ == "__main__":
